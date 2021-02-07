7b62f37ec3307cd7da1aade7a42becdd698a4486
zrabbbit <1374803499@qq.com>
2021-02-07 19:19:58 +0800

    second commit

--- src/School.java
+++ src/School.java
@@ -16,15 +16,15 @@ class Student
     }
     public String toString()
     {
-        return ("自我介绍：\n"+"姓名："+name+"\n学号："+id+"\n学院："+college);
+        return ("学生自我介绍：\n"+"姓名："+name+"\n学号："+id+"\n学院："+college);
     }
 }
-class Course
+abstract class Course
 {
-    private String name;
-    private String teacher;
-    private String site;
-    private List<Student> students;
+    protected String name;
+    protected String teacher;
+    protected String site;
+    protected List<Student> students;
     Course(){}
     Course(String name,String teacher,String site,List<Student> students)
     {
@@ -33,25 +33,50 @@ class Course
         this.site=site;
         this.students=students;
     }
-    public String toString()
-    {
-        System.out.println("课程介绍：");
-        System.out.println("课程名称："+name);
-        System.out.println("科任老师："+teacher);
-        System.out.println("上课地点："+site);
-        System.out.println("学生名单：");
-        int count=1;
-        for (Student i:students)
-        {
-            System.out.println("学生"+count);
-            System.out.println(i.toString());
-            count++;
-        }
-        return null;
-    }
+    public abstract String toString();
     public String getName()
     {
         return name;
+    }
+}
+class java_class extends Course
+{
+    java_class(){}
+    java_class(String name,String teacher,String site,List<Student> students)
+    {
+        super(name,teacher,site,students);
+    }
+    public String toString()
+    {
+        System.out.println("课程名称："+name);
+        System.out.println("任课老师："+teacher);
+        System.out.println("上课地点："+site);
+        System.out.println("参与课程学生：");
+        for (Student i:students)
+        {
+            System.out.println(i.toString());
+        }
+        return null;
+    }
+}
+class data_class extends Course
+{
+    data_class(){}
+    data_class(String name,String teacher,String site,List<Student> students)
+    {
+        super(name,teacher,site,students);
+    }
+    public String toString()
+    {
+        System.out.println("课程名称："+name);
+        System.out.println("任课老师："+teacher);
+        System.out.println("上课地点："+site);
+        System.out.println("参与课程学生：");
+        for (Student i:students)
+        {
+            System.out.println(i.toString());
+        }
+        return null;
     }
 }
 class Teacher
@@ -115,12 +140,43 @@ class Counselor extends Teacher implements Manage
         System.out.println("辅导员："+this.getName()+"所管理的学生个数为："+s.size()+"人"+"\n新增学生个数："+(s.size()-t));
     }
 }
+class Absence extends Exception
+{
+    private int sum;
+    Absence(int sum)
+    {
+        this.sum=sum;
+    }
+    public int getSum()
+    {
+        return sum;
+    }
+}
+class Learning
+{
+    private Course course;
+    private List<Student> students;
+    Learning(Course course,List<Student> students)
+    {
+        this.course = course;
+        this.students = students;
+    }
+    public void Roll_call() throws Absence
+    {
+        if(course.students.size()>students.size())
+        {
+            int shorting = course.students.size()-students.size();
+            throw new Absence(shorting);
+        }
+    }
+}
 public class School {
     public static void main(String[] args)
     {
         Student student_1 = new Student(1,"A","Math");
         System.out.println(student_1.toString());
         List<Student> students = new ArrayList<>();
+        List<Student> students1 = new ArrayList<>();
         students.add(student_1);
         Scanner scanner1 = new Scanner(System.in);
         while(scanner1.hasNextInt()) //学生列表 end退出
@@ -130,6 +186,7 @@ public class School {
             String college = scanner1.next();
             Student ss = new Student(id,name,college);
             students.add(ss);
+            students1.add(ss);
         }
         for (Student i:students)
         {
@@ -139,15 +196,27 @@ public class School {
         String c_name = scanner2.next();
         String t_name = scanner2.next();
         String site = scanner2.next();
-        Course course1 = new Course(c_name,t_name,site,students);
+        java_class course1 = new java_class(c_name,t_name,site,students);
         course1.toString();
+        data_class course2 = new data_class("data",t_name,site,students);
+        course2.toString();
         List<Course> courses = new ArrayList<>();
         courses.add(course1);
-        int id = scanner2.nextInt();
+        courses.add(course2);
+        /*int id = scanner2.nextInt();
         Teacher teacher = new Teacher(id,t_name,courses);
         teacher.toString();
         teacher.teaching();
         Counselor counselor = new Counselor(id,t_name,courses);
-        counselor.managing();
+        counselor.managing();*/
+        Learning class1 = new Learning(course1,students1);
+        try
+        {
+            class1.Roll_call();
+        }catch (Absence e)
+        {
+            System.out.println("该课程旷课人数为："+e.getSum());
+        }
+
     }
 }
